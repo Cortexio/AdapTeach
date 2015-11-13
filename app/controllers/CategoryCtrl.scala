e961dc2169668006c46922a8d65240c2430bfec5
Mikael Couzic <mikaelcouzic@gmail.com>
2015-11-13 21:58:35 +0100

    Category endpoint returns JSON

--- app/controllers/CategoryCtrl.scala
+++ app/controllers/CategoryCtrl.scala
@@ -5,15 +5,19 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 import play.api.mvc._
 import play.api.mvc.BodyParsers.parse
+import play.api.libs.json._
 
 import graph.CategoryRepo
+import models.Category
 
-class Category extends Controller {
+class CategoryCtrl extends Controller {
+
+	implicit val categoryWrites: Writes[Category] = Json.writes[Category]
 
 	def read(uuid: String) = Action.async { request =>
 		CategoryRepo.find(uuid) map { maybeCategory =>
 			maybeCategory match {
-				case Some(category) => Ok(category.toString) // TODO Respond with JSON by Writes
+				case Some(category) => Ok(Json.toJson(category)) // TODO Respond with JSON by Writes
 				case None => NotFound("No category found for uuid : " + uuid)
 			}
 		}
@@ -21,7 +25,7 @@ class Category extends Controller {
 
 	def create() = Action.async(parse.json) { request =>
 		CategoryRepo.create((request.body \ "name").as[String]) map { createdCategory =>
-			Ok(createdCategory.toString) // TODO Respond with JSON by Writes
+			Ok(Json.toJson(createdCategory)) // TODO Respond with JSON by Writes
 		}
 	}
 
