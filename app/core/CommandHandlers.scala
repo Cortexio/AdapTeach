eeeb0cf442a524a34b261507e11da20fb4806bd1
Mikael Couzic <mikaelcouzic@gmail.com>
2015-11-19 14:29:23 +0100

    Application Layers

--- app/core/CommandHandlers.scala
+++ app/core/CommandHandlers.scala
@@ -7,22 +7,40 @@ import core.commands._
 import core.commands.CreateItemHandler._
 import core.exceptions._
 
-trait SafetyLayer {
-	def handle[C <: Command, O <: Outcome[C]](command: C)(implicit handler: CommandHandler[C, O]): Future[O]
+trait ApplicationLayer {
+	def handle[C <: Command, O <: Outcome[C]](command: C, handler: CommandHandler[C, O]): Future[O]
+}
+
+trait SafetyLayer extends ApplicationLayer {
+	val nextLayer: ApplicationLayer
+}
+
+object CoreLayer extends ApplicationLayer {
+	def handle[C <: Command, O <: Outcome[C]](command: C, handler: CommandHandler[C, O]): Future[O] = {
+		 handler.handle(command)
+	}
 }
 
 object CommandHandlers {
 
 	def handle[C <: Command, O <: Outcome[C]](command: C)(implicit handler: CommandHandler[C, O]): Future[O] = {
-		Security.handle(command)
+		ValidationLayer.handle(command, handler)
 	}
 	
-	private object Security extends SafetyLayer {
-		def handle[C <: Command, O <: Outcome[C]](command: C)(implicit handler: CommandHandler[C, O]): Future[O] = command match {
-			case c: CreateItem => handler.handle(command) // TODO Check user is logged in
-			case _ => handler.handle(command)
+	private object ValidationLayer extends SafetyLayer {
+		val nextLayer = SecurityLayer
+		def handle[C <: Command, O <: Outcome[C]](command: C, handler: CommandHandler[C, O]): Future[O] = command match {
+			case c: CreateItem => nextLayer.handle(command, handler) // TODO Check user is logged in
+			case _ => nextLayer.handle(command, handler)
 		}
 	}
-	
+
+	private object SecurityLayer extends SafetyLayer {
+		val nextLayer = CoreLayer
+		def handle[C <: Command, O <: Outcome[C]](command: C, handler: CommandHandler[C, O]): Future[O] = command match {
+			case c: CreateItem => nextLayer.handle(command, handler) // TODO Check user is logged in
+			case _ => nextLayer.handle(command, handler)
+		}
+	}
 
 }
